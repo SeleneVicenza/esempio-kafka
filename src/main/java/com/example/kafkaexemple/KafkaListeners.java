7f9b7b60bccb7978ad57c72c0fb3e775bcd33a9b
vinda <vindamelio@yahoo.it>
2025-01-26 22:30:27 +0100

    esempio kafka

---
+++ src/main/java/com/example/kafkaexemple/KafkaListeners.java
@@ -0,0 +1,16 @@
+package com.example.kafkaexemple;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+	
+	@KafkaListener(
+			topics= "amigoscode", 
+			groupId = "groupId"
+	)
+	void listener(String data) {
+		System.out.println("Recived: " + data + " 🎉");
+	}
+}
---
+++ src/main/java/com/example/kafkaexemple/MessageRequest.java
@@ -0,0 +1,5 @@
+package com.example.kafkaexemple;
+
+public record MessageRequest(String message) {
+
+}
